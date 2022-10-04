b8fcbf721b1b93147cb376f85b1f641bd89b7eb5
FrancoLedi <francoivanledesma551@gmail.com>
2022-10-04 20:32:17 -0300

    Al fin terminado Project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './Projects.css';
 import { ProjectCard } from './ProjectCard';
+import FoodAPP from '../images/FoodAPP.png';
+import HenryHardware from '../images/HenryHardware.jpg';
 
 export const Projects = () => {
   return (
     <>
     <Container fluid className='projects'>
         <h1 className='title text-center'>Projects</h1>
-        <Col >
-            <Row >
+        <Row className=''>
+            <Col className='col-md-12' >
               
-                <ProjectCard />
+                <ProjectCard title='FoodAPP' image={FoodAPP} paragraph='Este proyecto se desarrollo utilizando React.js desde el Client-Side en conjunto con Redux para el manejo de estados ademas de Node.js y Express para el back por ultimo aclarar que programe una base de datos PostgreSQL haciendo uso de Sequelize.'/>
               
-            </Row>
-            <Row >
+            </Col>
+            <Col className='col-md-12 '>
               
-                <ProjectCard />
+            <ProjectCard title='HenryHardware' image={HenryHardware} paragraph='Este proyecto es un e-commerce que desarrolle en un equipo con otros 5 desarrrolladores haciendo uso de metodologias agiles (SCRUM)'/>
               
-            </Row>
-        </Col>
+            </Col>
+        </Row>
     </Container>
     </>
   )

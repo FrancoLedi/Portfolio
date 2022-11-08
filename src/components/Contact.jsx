0b97d16b15a4076f37bb5cd1520692337eb3bdbc
FrancoLedi <francoivanledesma551@gmail.com>
2022-11-08 14:15:46 -0300

    Empiezo con las validaciones

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -53,7 +53,7 @@ export const Contact = () => {
           </Col>
           <Col size={12} md={6}>
             
-                <h2>Contact Me</h2>
+                <h2>{language ? 'Contactá conmigo' : 'Contact Me'}</h2>
                 <form onSubmit={handleSubmit}>
                   <Row>
                     <Col size={12} sm={6} className="px-1 mt-1">

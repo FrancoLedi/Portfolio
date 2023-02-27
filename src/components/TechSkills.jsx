fc4d3e3fd45ea450428de05a5f3834a101985147
FrancoLedi <francoivanledesma551@gmail.com>
2023-02-27 13:40:40 -0300

    Actualizados logos de PostCSS y Railway ademas de agregar height: 75% en Home.css

--- src/components/TechSkills.jsx
+++ src/components/TechSkills.jsx
@@ -15,16 +15,16 @@ export const TechSkills = () => {
     <Row className= 'd-flex flex-row mt-3'>
         <Col xxl={3}  xl={3}  lg={3}  md={3}  sm={6}  xs={6} className='text-center d-flex flex-column align-items-center col'>
             
-            <span className='d-flex flex-column'><Icon icon="logos:html-5" width="110" height="100" />Html</span>
+            <span className='d-flex flex-column'><Icon icon="logos:html-5" width="110" height="100" />HTML</span>
             <span className='d-flex flex-column'><Icon icon="logos:bootstrap" width="110" height="100" />Bootstrap</span>
             <span className='d-flex flex-column'> <Icon icon="logos:javascript" width="110" height="100" />Javascript</span>
-            <span className='d-flex flex-column'> <Icon icon="logos:sass" width="110" height="100" />Sass</span>
+            <span className='d-flex flex-column'> <Icon icon="vscode-icons:file-type-postcss" width="110" height="100" />PostCSS</span>
         </Col>
         <Col xxl={3}  xl={3}  lg={3}  md={3}  sm={6}  xs={6} className='text-center d-flex flex-column align-items-center col'>
-            <span className='d-flex flex-column'> <Icon icon="logos:css-3" width="110" height="100" />Css</span>
+            <span className='d-flex flex-column'> <Icon icon="logos:css-3" width="110" height="100" />CSS</span>
             <span className='d-flex flex-column'> <Icon icon="logos:nodejs" width="110" height="100" />Node</span>
             <span className='d-flex flex-column'> <Icon icon="logos:sequelize" width="110" height="100" />Sequelize</span>
-            <span className='d-flex flex-column'> <Icon icon="logos:heroku-icon" width="110" height="100" />Heroku</span>
+            <span className='d-flex flex-column'> <Icon icon="simple-icons:railway" width="110" height="100" />Railway</span>
         </Col>
         <Col xxl={3}  xl={3}  lg={3}  md={3}  sm={6}  xs={6} className='text-center d-flex flex-column align-items-center col'>
             <span className='d-flex flex-column'> <Icon icon="logos:postgresql" width="110" height="100" />Postgresql</span>
